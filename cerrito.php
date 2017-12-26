c6759d72dc5f2e7f094b4a944175bb8613f576ac
marianoleguizamon84 <mariano@P-000004-CR.AUSTRAL.ACES>
2017-12-26 15:27:31 -0300

    Responsibe pantalla

--- cerrito.php
+++ cerrito.php
@@ -49,12 +49,26 @@ $conn = null;
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <script type="text/javascript">
     window.onload = function() {
+      encabezado();
       calculos();
       cambioAltura(filaAltura);
-      cambio();
-      window.setInterval(cambio, 10000);
+      if ($(window).width() > 500) {
+        cambio();
+        // window.setInterval(cambio, 10000);
+        window.setInterval(cambio, 10000);
+      }
+      var titulo = $("#encabezado")
+      titulo.hide();
+      $(window).scroll(function(){
+      if($(window).scrollTop() >= 100 ){
+        titulo.show();
+      } else {
+        titulo.hide();
+      }
+      });
     }
 
     // window.onresize = function(event){
@@ -63,6 +77,14 @@ $conn = null;
     //   cambio();
     //   // window.setInterval(cambio, 10000);
     // }
+    function encabezado(){
+      var tablaTitulo = document.getElementsByClassName('tablaTitulo');
+      var encabezadoTitulo = document.getElementsByClassName('encabezadoTitulo');
+      for (var i = 0; i < tablaTitulo.length; i++) {
+        encabezadoTitulo[i].style.width = tablaTitulo[i].offsetWidth + "px";
+        encabezadoTitulo[i].style.height = tablaTitulo[i].offsetHeight + "px";
+      }
+    }
 
     function calculos(){
       var largo = window.innerHeight;
@@ -72,26 +94,20 @@ $conn = null;
       cursos = (document.getElementsByTagName('tr')) ;
       var cursosCant = (document.getElementsByTagName('tr').length) - 1 ;
       pasadas = Math.ceil(cursosCant/tanda);
-      cont = 1;
-      // console.log("Titulo: " + filaTitulo);
-      // console.log("cada Fila: " + filaAltura);
-      // console.log("Largo Pagina: " + largo);
-      // console.log("Entran: " + tanda);
-      // console.log("Cursos: " + cursosCant);
-      // console.log("Pasadas: " + pasadas);
-      // console.log('Pasada '+ cont);
-      // console.log('Maxima altura de TD ' + filaAltura + 'px');
+      cont = 0;
     }
 
     function cambio(){
       var desaparecer = [];
       var aparecer = [];
+      cont++;
+      if (cont >= pasadas) {
+        cont = 1;
+      }
       for (var i = 1; i < cursos.length; i++) {
         if ((i >= tanda*cont - (tanda-1)) && (i <= tanda*cont)) {
-          // console.log(i);
           aparecer.push(cursos[i]);
         } else {
-          // console.log(i + " X");
           desaparecer.push(cursos[i]);
         }
       }
@@ -103,13 +119,8 @@ $conn = null;
           var x = $(aparecer[i]).show('slow');
         }
       }, 1000);
-
-      cont = cont + 1;
-      // console.log("--> " + cont + " --> " + pasadas);
-      if (cont > pasadas) {
-        cont = 1;
-        }
       }
+
 
       function medio(){
         var maxHeight = 0;
@@ -124,10 +135,7 @@ $conn = null;
       }
 
       function cambioAltura(max){
-        // var fila = document.getElementsByTagName('td');
-
-        for (var i = 1; i < cursos.length; i++) {
-          // fila[i].style.height = max + "px";
+        for (var i = 1; i < cursos.length - 1; i++) {
           cursos[i].style.height = max + "px";
         }
       }
@@ -163,8 +171,22 @@ $conn = null;
       font-size: 50px;
     }
     .botonera{
-      display: flex;
+      display: none;
+      position: fixed;
+      bottom: 0;
       align-content: space-between;
+      width: 100%;
+    }
+    .encabezado{
+      /* display: none; */
+      position: fixed;
+      top: 0;
+      background-color: rgba(8,45,88,1);
+      border-bottom: solid 1px white;
+    }
+    .encabezado th{
+      padding: 8px;
+      border-bottom: 2px;
     }
     .flechas{
       background-color: Transparent;
@@ -175,21 +197,38 @@ $conn = null;
     }
     input{
       text-align: center;
-      width: 50%;
+      /* width: 50%; */
+      width: 100%;
       border-radius: 5px;
       box-sizing: border-box;
+      font-size: 25px;
+    }
+    @media (max-width: 500px) {
+      body{
+        font-size: 15px;
+      }
+      .botonera {
+        display: flex;
+      }
+      table {
+        font-size: 17px;
+        color: white;
+      }
+      th {
+        font-size: 25px;
+      }
     }
     </style>
   </head>
-  <body style="">
+  <body>
     <table class="table">
       <thead>
-        <th>Curso</th>
-        <th>Inicio</th>
-        <th>Fin</th>
-        <th>Aula</th>
+        <th class="tablaTitulo">Curso</th>
+        <th class="tablaTitulo">Inicio</th>
+        <th class="tablaTitulo">Fin</th>
+        <th class="tablaTitulo">Aula</th>
       </thead>
-      <tbody>
+    <tbody>
         <?php foreach ($result as $value) {
           echo "<tr>";
           echo "<td>" . $value['name'] . "</td>";
@@ -200,10 +239,20 @@ $conn = null;
         } ?>
         </tbody>
     </table>
-    <div class="botonera">
-      <button type="button" name="button" class="flechas" onclick="atras()"><span class="glyphicon glyphicon-chevron-left"></span></button>
+    <div class="encabezado" id="encabezado">
+      <table>
+        <thead>
+          <th class="encabezadoTitulo">Curso</th>
+          <th class="encabezadoTitulo">Inicio</th>
+          <th class="encabezadoTitulo">Fin</th>
+          <th class="encabezadoTitulo">Aula</th>
+        </thead>
+      </table>
+    </div>
+    <div class="botonera" id="botonera">
+      <!-- <button type="button" name="button" class="flechas" onclick="atras()"><span class="glyphicon glyphicon-chevron-left"></span></button> -->
       <input type="date" name="" value="<?php echo $anio."-".$mes."-".$dia ?>" onchange="calendar()">
-      <button type="button" name="button" class="flechas" onclick="adelante()"><span class="glyphicon glyphicon-chevron-right"></span></button>
+      <!-- <button type="button" name="button" class="flechas" onclick="adelante()"><span class="glyphicon glyphicon-chevron-right"></span></button> -->
     </div>
 
   </body>
